06c3dd2ee1799b20e72e08053b90db2c2289cc05
lperthel <lperthel@gmail.com>
2023-05-22 18:58:03 -0400

    Update HTML parser to use a URL instead of a file on the file system		initial commit to create branch on remote

--- src/main/java/com/lperthel/HTMLReader/HTMLDocumentParserByHeading.java
+++ src/main/java/com/lperthel/HTMLReader/HTMLDocumentParserByHeading.java
@@ -18,74 +18,72 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class HTMLDocumentParserByHeading{
-	private static String HEADING_TAG = "h3";
-	private static String OUTPUT_DIRECTORY = "C:\\Users\\lpert\\OneDrive\\Documents\\ITCareer\\Hibernate Reference Documents\\User Guide";
-	public static String HTML_FILE = null;
+public class HTMLDocumentParserByHeading {
+    private static String OUTPUT_DIRECTORY = "C:\\Users\\lpert\\OneDrive\\Documents\\ITCareer\\Hibernate Reference Documents\\User Guide";
+    private static String HTML_URL = "https://docs.jboss.org/hibernate/orm/6.2/userguide/html_single/Hibernate_User_Guide.html";
+    private static String HEADING_TAG = "h3";
 
-public static void main(String[] args) {
-    final Path outputDir = Paths.get(OUTPUT_DIRECTORY);
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        final Path outputDir = Paths.get(OUTPUT_DIRECTORY);
+        Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Enter the heading name to extract contents: ");
-    String headingName = scanner.nextLine();
+        System.out.print("Enter the heading name to extract contents: ");
+        String headingName = scanner.nextLine();
 
-    try {
-        Document doc = Jsoup.parse(Paths.get(HTML_FILE).toFile(), "UTF-8", "");
-        Elements headings = doc.select(HEADING_TAG);
+        try {
+            Document doc = Jsoup.connect(HTML_URL).get();
+            Elements headings = doc.select(HEADING_TAG);
 
-        Map<String, String> sections = new HashMap<>();
+            Map<String, String> sections = new HashMap<>();
 
-        for (Element heading : headings) {
-            String sectionTitle = heading.text();
-            if (sectionTitle.equalsIgnoreCase(headingName)) {
-                StringBuilder sectionContent = new StringBuilder();
-                Element next = heading.nextElementSibling();
-                while (next != null && !next.tagName().equals(HEADING_TAG)) {
-                    sectionContent.append(next.outerHtml());
-                    next = next.nextElementSibling();
+            for (Element heading : headings) {
+                String sectionTitle = heading.text();
+                if (sectionTitle.equalsIgnoreCase(headingName)) {
+                    StringBuilder sectionContent = new StringBuilder();
+                    Element next = heading.nextElementSibling();
+                    while (next != null && !next.tagName().equals(HEADING_TAG)) {
+                        sectionContent.append(next.outerHtml());
+                        next = next.nextElementSibling();
+                    }
+                    String sectionText = Jsoup.parse(sectionContent.toString()).text();
+                    sections.put(sectionTitle, sectionText);
+                    break; // Stop parsing when the desired heading is found
                 }
-                String sectionText = Jsoup.parse(sectionContent.toString()).text();
-                sections.put(sectionTitle, sectionText);
-                break; // Stop parsing when the desired heading is found
             }
-        }
 
-        if (sections.isEmpty()) {
-            System.out.println("No sections found with the specified heading name.");
-        } else {
-            for (Map.Entry<String, String> section : sections.entrySet()) {
-                String title = section.getKey();
-                String content = section.getValue();
+            if (sections.isEmpty()) {
+                System.out.println("No sections found with the specified heading name.");
+            } else {
+                for (Map.Entry<String, String> section : sections.entrySet()) {
+                    String title = section.getKey();
+                    String content = section.getValue();
 
-                // create a new docx file with section content
-                XWPFDocument docx = new XWPFDocument();
-                XWPFParagraph para = docx.createParagraph();
-                para.setAlignment(ParagraphAlignment.LEFT);
+                    // create a new docx file with section content
+                    XWPFDocument docx = new XWPFDocument();
+                    XWPFParagraph para = docx.createParagraph();
+                    para.setAlignment(ParagraphAlignment.LEFT);
 
-                XWPFRun run = para.createRun();
-                run.setFontSize(35); // Set the font size to 35
-                run.setText(content);
+                    XWPFRun run = para.createRun();
+                    run.setFontSize(30); // Set the font size to 30
+                    run.setText(content);
 
-                Path outputPath = outputDir.resolve(title + ".docx");
+                    Path outputPath = outputDir.resolve(title + ".docx");
 
-                System.out.println("Opening file for writing: " + outputPath.toString());
+                    System.out.println("Opening file for writing: " + outputPath.toString());
 
-                Files.createDirectories(outputPath.getParent());
-                try (BufferedOutputStream out = new BufferedOutputStream(Files.newOutputStream(outputPath))) {
-                    docx.write(out);
+                    Files.createDirectories(outputPath.getParent());
+                    try (BufferedOutputStream out = new BufferedOutputStream(Files.newOutputStream(outputPath))) {
+                        docx.write(out);
+                    }
+
+                    System.out.println("File written successfully: " + outputPath.toString());
                 }
-
-                System.out.println("File written successfully: " + outputPath.toString());
             }
+        } catch (IOException e) {
+            System.err.println("Error while parsing HTML content: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            scanner.close();
         }
-
-    } catch (IOException e) {
-        System.err.println("Error while parsing HTML file: " + e.getMessage());
-        e.printStackTrace();
-    } finally {
-        scanner.close();
     }
-}
-
 }
